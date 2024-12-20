80dab1d034433178289fd0cf70e27e2c2d63db95
Apil120 <apilisadhikari@gmail.com>
2024-12-20 22:57:34 +0545

    Deleted all for some reason lol

--- myproject/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-# from django.http import HttpResponse
-# Create your views here.
-def helloworld(request):
-    context = {
-        "message":"Hello From Template"
-    }
-    return render(request,"hello.html",context)
