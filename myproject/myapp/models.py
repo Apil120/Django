4f1cbf3eefc1f54f236db8c2a5e3ae4040ce3b95
Apil120 <apilisadhikari@gmail.com>
2025-01-01 11:02:57 +0545

    DB updated, new migration created.

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -5,6 +5,8 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     price = models.DecimalField(max_digits=8,decimal_places=5,help_text="Price of book in this format:123.45")#max_digits le overall kati ota digits hunchha tyo control garchha, decimal_places le chai decimal point pachi kati ota digit rakhne bhanne kura handle garcha 
+    genre = models.CharField(max_length=100,default="Fiction")
+
     id = models.IntegerField(primary_key=True,auto_created=True) 
 
     def __str__(self):
