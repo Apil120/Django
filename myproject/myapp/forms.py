254831d7d99a4d40ef0dce0de8f66f25cefd2b02
Apil120 <apilisadhikari@gmail.com>
2025-01-01 15:45:35 +0545

    Changes to Schema, Migration added

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -4,4 +4,10 @@ from .models import Book
 class BookForm(forms.ModelForm):
     class Meta: #Configuration settings of the model(configuration settings bannale db ma kun kun field baschha kun model sanga tied hunchha bhanne kura ho !)
         model = Book
-        fields = ['title', 'author', 'published_date','price','id']
+        fields = ['title', 'author', 'published_date','price','id','genre']
+
+    def clean_genre(self):
+        genres = self.cleaned_data['genre']
+
+        if isinstance(genres,str):
+            return [genre.strip() for genre in genres.split(",") if genre.strip()]
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     price = models.DecimalField(max_digits=8,decimal_places=5,help_text="Price of book in this format:123.45")#max_digits le overall kati ota digits hunchha tyo control garchha, decimal_places le chai decimal point pachi kati ota digit rakhne bhanne kura handle garcha 
-    genre = models.CharField(max_length=100,default="Fiction")
+    genre = models.CharField(null=False,blank=True,max_length=100)
 
     id = models.IntegerField(primary_key=True,auto_created=True) 
 
