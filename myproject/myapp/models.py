90f85739f53315762e0b7d606620de1d56a52a37
Apil120 <apilisadhikari@gmail.com>
2025-01-01 17:07:00 +0545

    Added Library model and migratioed

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -8,7 +8,13 @@ class Book(models.Model):
     price = models.DecimalField(max_digits=8,decimal_places=5,help_text="Price of book in this format:123.45")#max_digits le overall kati ota digits hunchha tyo control garchha, decimal_places le chai decimal point pachi kati ota digit rakhne bhanne kura handle garcha 
     genre = models.CharField(null=False,blank=True,max_length=100)
 
-    id = models.IntegerField(primary_key=True,auto_created=True) 
 
     def __str__(self):
         return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    available_books = models.ManyToManyField(Book,related_name="libraries")
+
+    def __str__(self):
+        return self.name
