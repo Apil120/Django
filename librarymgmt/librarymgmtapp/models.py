534a30db34fec5428285918ed13841ed15954729
Apil120 <apilisadhikari@gmail.com>
2025-01-03 00:12:31 +0545

    Validation changed

--- librarymgmt/librarymgmtapp/models.py
+++ librarymgmt/librarymgmtapp/models.py
@@ -1,20 +1,20 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
-def validate_status(value):
-    if value  in ("available","unavailable","borrowed"):
-        return value
-    
-    raise ValidationError("The status should be one of ['available','unavailable','borrowed']")
+
         
     
 class Book(models.Model):
+    STATUS_CHOICES = [
+        ("available", "Available"),
+        ("unavailable", "Unavailable"),
+        ("borrowed", "Borrowed"),
+    ]
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     genre = models.CharField(max_length=100)
     count = models.IntegerField()
-    status = models.CharField(max_length=100,validators=validate_status)
+    status = models.CharField(max_length=100,choices=STATUS_CHOICES)
 
     def __str__(self):
         return self.title
