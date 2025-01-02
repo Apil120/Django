79968cc2e12939dfe68b313a9808cad347bf3caa
Apil120 <apilisadhikari@gmail.com>
2025-01-03 00:08:30 +0545

    Book model created with a validator

--- librarymgmt/librarymgmtapp/models.py
+++ librarymgmt/librarymgmtapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+def validate_status(value):
+    if value  in ("available","unavailable","borrowed"):
+        return value
+    
+    raise ValidationError("The status should be one of ['available','unavailable','borrowed']")
+        
+    
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+    genre = models.CharField(max_length=100)
+    count = models.IntegerField()
+    status = models.CharField(max_length=100,validators=validate_status)
+
+    def __str__(self):
+        return self.title
+    
