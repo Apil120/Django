1141eeaddcb5fb5844f1610530e8d8144bb95ef6
Apil120 <apilisadhikari@gmail.com>
2025-01-13 15:44:43 +0545

    Minor spelling fix.

--- librarymgmt/librarymgmt/settings.py
+++ librarymgmt/librarymgmt/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'librarymgmtapp',
-    'djangp.contrib.sites',
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
