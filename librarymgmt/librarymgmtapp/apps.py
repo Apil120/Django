8502f407d3d9ade4c1a41af7aff76d7157a89b8e
Apil120 <apilisadhikari@gmail.com>
2025-01-14 16:47:35 +0545

    Remove EVERYTHING.

--- librarymgmt/librarymgmtapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibrarymgmtappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'librarymgmtapp'
--- librarymgmt/librarymgmtapp/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import User
-from django import forms
-
-class Userform(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['name','email','member_id','borrowed_books']
-
